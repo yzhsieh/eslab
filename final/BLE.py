208b7b2acef05008a3675874e7eb8a846e2d4498
jack hsieh <jack hsieh>
2018-01-11 17:10:18 +0800

    add BLE command

--- final/BLE.py
+++ final/BLE.py
@@ -10,10 +10,49 @@ BLE = None
 class BLEdevice:
 	def __init__(self):
 		device = None
+		fanCurrentPower = 'off'
+		fanCurrentSpeed = 0.5
+		fanPowerPin = 3
+		fanPostivePin = 5
+		fanNegativePin = 6
+
+		connect()
+		set_pin_mode_digital(fanPowerPin)
+		set_pin_mode_PWM(fanPostivePin)
+		set_pin_mode_PWM(fanNegativePin)
 
 	def connect(self):
 		device = pexpect.spawn(CMD_CONNECT,logfile=None, searchwindowsize=200)
 
+	def fanPowerSwitch(self):
+		if fanCurrentPower == 'on':
+			send_pin_value_digital(fanPowerPin, 0)
+			fanCurrentPower = 'off'
+		else:
+			send_pin_value_digital(fanPowerPin, 1)
+			time.sleep(0.5)
+			send_pin_value_PWM(fanPostivePin, 0.8*255)
+			time.sleep(0.3)
+			send_pin_value_PWM(fanPostivePin, fanCurrentSpeed*255)
+			
+	def speedUpFan(self):
+		if fanCurrentPower == 'off':
+			return 'err0' # fan is not on
+		else:
+			if fanCurrentSpeed < 1:
+				fanCurrentSpeed += 0.1
+				send_pin_value_PWM(fanPostivePin, fanCurrentSpeed*255)
+			else:
+				return 'err1' # fan is full speed
+	def speedDownFan(self):
+		if fanCurrentPower == 'off':
+			return 'err0' # fan is not on
+		else:
+			if fanCurrentSpeed > 0.1:
+				fanCurrentSpeed -= 0.1
+				send_pin_value_PWM(fanPostivePin, fanCurrentSpeed*255)
+			else:
+				return 'err1' # fan is lowest speed
 
 	def set_pin_mode_digital(self, pin):
 		cmd = 'char-write-cmd 0x000e 0x53{:02x}01'.format(pin)
