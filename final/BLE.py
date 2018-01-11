7c20c44a932a36d2f1dd38656c7e57deaab40ed5
jack hsieh <jack hsieh>
2018-01-11 15:49:33 +0800

    turn BLE.py into a class base function

--- final/BLE.py
+++ final/BLE.py
@@ -7,26 +7,36 @@ initialization_timeout = 3
 CMD_CONNECT = 'sudo gatttool -b D5:3A:73:E3:F0:7E -t random --interactive'
 BLE = None
 ###
-def set_pin_mode_digital(pin):
-	cmd = 'char-write-cmd 0x000e 0x53{:02x}01'.format(pin)
-	BLE.sendline(cmd)
+class BLEdevice:
+	def __init__(self):
+		device = None
 
-def set_pin_mode_PWM(pin):
-	cmd = 'char-write-cmd 0x000e 0x53{:02x}03'.format(pin)
-	BLE.sendline(cmd)
+	def connect(self):
+		device = pexpect.spawn(CMD_CONNECT,logfile=None, searchwindowsize=200)
 
-def send_pin_value_PWM(pin, value):
-	cmd = 'char-write-cmd 0x000e 0x4E{:02x}{:02x}'.format(pin, value)
-	# print(cmd)
-	BLE.sendline(cmd)
 
-def send_pin_value_digital(pin, value):
-	cmd = 'char-write-cmd 0x000e 0x54{:02x}{:02x}'.format(pin, value)
-	print(cmd)
-	BLE.sendline(cmd)
+	def set_pin_mode_digital(self, pin):
+		cmd = 'char-write-cmd 0x000e 0x53{:02x}01'.format(pin)
+		BLE.sendline(cmd)
+
+	def set_pin_mode_PWM(self, pin):
+		cmd = 'char-write-cmd 0x000e 0x53{:02x}03'.format(pin)
+		BLE.sendline(cmd)
+
+	def send_pin_value_PWM(self, pin, value):
+		cmd = 'char-write-cmd 0x000e 0x4E{:02x}{:02x}'.format(pin, value)
+		# print(cmd)
+		BLE.sendline(cmd)
+
+	def send_pin_value_digital(self, pin, value):
+		cmd = 'char-write-cmd 0x000e 0x54{:02x}{:02x}'.format(pin, value)
+		print(cmd)
+		BLE.sendline(cmd)
 
 if __name__ == '__main__':
 	print("START")
+
+	## 下面還沒改
 	BLE = pexpect.spawn(CMD_CONNECT,logfile=None, searchwindowsize=200)
 	BLE.sendline('connect')
 	print("Connect success")
